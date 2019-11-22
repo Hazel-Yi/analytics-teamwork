d83f5b9af23e9af085e25265a30fbe7a069de44a
Niriksha <niriksha.mutharia@gmail.com>
2019-11-22 21:49:11 +1100

    progress

--- client-side/app/routes.py
+++ client-side/app/routes.py
@@ -9,8 +9,8 @@ def get_boardgame():
   r = requests.get("http://127.0.0.1:8000/details", params={'order': 'Game_ID', 'ascending':True})
   games = r.json()
 
-  h = games[1]
-  print(h)
+  testing = games[1]
+  '''print(testing)'''
 
   listlist = []
   game_id_list = []
@@ -34,9 +34,9 @@ def get_boardgame():
     sublist.append(year)
     listlist.append(sublist)
 
-  print(listlist[0])
+  '''print(listlist[0])'''
 
-  return render_template('get_boardgame.html', title='Get Boardgames', listlist=listlist, game_id_list=game_id_list[0:10], name_list=name_list[0:10], year_list=year_list[0:10])
+  return render_template('get_boardgame.html', title='All Boardgames', listlist=listlist, game_id_list=game_id_list[0:10], name_list=name_list[0:10], year_list=year_list[0:10])
     
 
 @app.route('/get_boardgame_id', methods=['GET', 'POST'])
@@ -57,10 +57,86 @@ def get_boardgame_id():
         description = str(game[key])
       if key == 'Expansion':
         expansions = str(game[key])
-    print(name)
-    return render_template('get_boardgame_id_2.html', title='Get Boardgame ID', name=name, published_by=published_by, categories=categories, description=description, expansions=expansions)
+    '''print(name)'''
+    return render_template('get_boardgame_id_2.html', title='Find Boardgame', name=name, published_by=published_by, categories=categories, description=description, expansions=expansions)
   else:
-    return render_template('get_boardgame_id.html', title='Get Boardgame ID')
+    return render_template('get_boardgame_id.html', title='Find Boardgame')
+
+@app.route('/post_boardgame', methods=['GET', 'POST'])
+def post_boardgame():
+  if request.method == 'POST':
+    published_by = []
+    categories = []
+    expansions = []
+    mechanics = []
+    game_id = request.form['id']
+    name = request.form['name']
+    published_by.append(request.form['published_by'])
+    categories.append(request.form['categories'])
+    min_players = request.form['min_players']
+    max_players = request.form['max_players']
+    min_age = request.form['min_age']
+    min_playtime = request.form['min_playtime']
+    description = request.form['description']
+    expansions.append(request.form['expansions'])
+    mechanics.append(request.form['mechanics'])
+    thumbnail = request.form['thumbnail']
+    year = request.form['year']
+    '''print(name)'''
+    game = {
+      'Game_ID': int(game_id),
+      'Name': name,
+      'Publisher': published_by,
+      'Category': categories,
+      'Min_players': int(min_players),
+      'Max_players': int(max_players),
+      'Min_age': int(min_age),
+      'Min_playtime': int(min_playtime),
+      'Description': description,
+      'Expansion': expansions,
+      'Mechanic': mechanics,
+      'Thumbnail': thumbnail,
+      'Year_Published': int(year)
+    }
+    '''print(game)'''
+    
+    r = requests.post("http://127.0.0.1:8000/details", json=game)
+
+    print("Status Code:" + str(r.status_code))
+    resp = r.json()
+
+    print(resp['message'])
+
+  return render_template('post_boardgame.html', title='Add a Boardgame')
+
+@app.route('/put_boardgame', methods=['GET', 'POST'])
+def put_boardgame():
+  if request.method == 'POST':
+    published_by = []
+    categories = []
+    expansions = []
+    mechanics = []
+    game_id = request.form['id']
+    name = request.form['name']
+    published_by.append(request.form['published_by'])
+    categories.append(request.form['categories'])
+    min_players = request.form['min_players']
+    max_players = request.form['max_players']
+    min_age = request.form['min_age']
+    min_playtime = request.form['min_playtime']
+    description = request.form['description']
+    expansions.append(request.form['expansions'])
+    mechanics.append(request.form['mechanics'])
+    thumbnail = request.form['thumbnail']
+    year = request.form['year']
+    
+    print("searched for a game with id " + game_id)
+
+    r = requests.put("http://127.0.0.1:8000/details/" + str(game_id))
+    game = r.json()
+    print(game)
+
+  return render_template('put_boardgame.html', title='Update a Boardgame')
 
 @app.route('/get_rec', methods=['GET', 'POST'])
 def get_rec():
@@ -69,53 +145,45 @@ def get_rec():
     search_id = request.form['search']
     num_rec = request.form['num_rec']
     print("searched for a game with id " + search_id + " and " + num_rec + " number of rec")
-    return render_template('get_rec_2.html', title='Get Recommendation', num_rec=num_rec) 
+    return render_template('get_rec_2.html', title='Recommended Games', num_rec=num_rec) 
   else:
-    return render_template('get_rec.html', title='Get Recommendation')
+    return render_template('get_rec.html', title='Recommended Games')
 
-@app.route('/post_boardgame', methods=['GET', 'POST'])
-def post_boardgame():
+@app.route('/')
+@app.route('/get_review')
+def get_review():
+  return render_template('get_review.html', title='Find Reviews')
+
+@app.route('/post_review', methods=['GET', 'POST'])
+def post_review():
   if request.method == 'POST':
     game_id = request.form['id']
-    name = request.form['name']
-    published_by = request.form['published_by']
-    categories = request.form['categories']
-    min_players = request.form['min_players']
-    max_players = request.form['max_players']
-    min_age = request.form['min_age']
-    min_playtime = request.form['min_playtime']
-    description = request.form['description']
-    expansions = request.form['expansions']
-    mechanics = request.form['mechanics']
-    thumbnail = request.form['thumbnail']
-    year = request.form['year']
-    print(name)
-    game = {
-      'Game_ID': game_id,
-      'Name': name,
-      'Publisher': published_by,
-      'Category': categories,
-      'Min_players': min_players,
-      'Max_players': max_players,
-      'Min_age': min_age,
-      'Min_playtime': min_playtime,
-      'Description': description,
-      'Expansion': expansions,
-      'Mechanic': mechanics,
-      'Thumbnail': thumbnail,
-      'Year_Published': year
-    }
-    print(game)
+    username = request.form['username']
+    rating = request.form['rating']
+    comment = request.form['comment']
 
-    r = requests.post("http://127.0.0.1:8000/details", json=game)
+    review = {
+      'Game_ID': int(game_id),
+      'User': username,
+      'Rating': float(rating),
+      'Comment': comment,
+    }
+    
+    print(review)
+
+    auth = requests.get("http://127.0.0.1:8000/auth")
+    token = auth.json()
+
+    print(token)
+    for key in token.keys():
+      t = str(token[key])
+      
+    print(t)
+
+    r = requests.post("http://127.0.0.1:8000/reviews", json=review, headers={'AUTH-TOKEN': t})
 
     print("Status Code:" + str(r.status_code))
     resp = r.json()
 
     print(resp['message'])
-
-  return render_template('post_boardgame.html', title='Post Boardgame')
-
-@app.route('/put_boardgame')
-def put_boardgame():
-  return render_template('put_boardgame.html', title='Put Boardgame')
+  return render_template('post_review.html', title='Add a Review')
