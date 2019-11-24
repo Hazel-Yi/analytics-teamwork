df43a946036cb99441e955b1b4816ec2491d93fa
Samer Haddad <samer28.haddad@hotmail.com>
2019-11-25 09:46:54 +1100

    Updated get_price to try all sites

--- webscraper.py
+++ webscraper.py
@@ -24,22 +24,28 @@ game = 'Azul'
 #game = 'New Bedford'
 
 
-def get_price(game, ext=False, loc='au'):
+def get_price(game, ext=False):
     price_bg = get_bg_price(game, ext)
-    price_amazon = get_amazon_price(game, loc, ext)
     if price_bg:
         return price_bg
-    elif price_amazon:
-        return price_amazon
+    
+    price_amazon_au = get_amazon_price(game, 'au', ext)
+    if price_amazon_au:
+        return price_amazon_au
+    
+    price_amazon_us = get_amazon_price(game, 'us', ext)
+    if price_amazon_us:
+        return price_amazon_us
+
     else:
         return False
 
-def get_all_prices(game, ext=False, loc='au'):
+def get_all_prices(game, loc='au', ext=False):
     prices = []
     prices.append(get_bg_price(game, ext))
     prices.append(get_amazon_price(game, loc, ext))
     return prices
 
 if __name__ == '__main__':
-    price = get_all_prices(game, True, 'us')
+    price = get_price(game, True)
     print(price)
