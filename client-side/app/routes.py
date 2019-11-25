15d229f4c6a79a28fa2d5a92ba86ba0b67303110
Jerry Zhu <jerrybzhu@gmail.com>
2019-11-25 16:46:53 +1100

    added  /get_boardgame_id/<int:id> route

--- client-side/app/routes.py
+++ client-side/app/routes.py
@@ -77,7 +77,8 @@ def get_boardgame():
 def find_boardgame():
   return render_template('find_boardgame.html', title='Find Boardgame')
 
-@app.route('/get_boardgame_id', methods=['GET', 'POST'])
+
+@app.route('/get_boardgame_id/', methods=['GET', 'POST'])
 def get_boardgame_id():
   if request.method == 'POST':
     search_id = request.form['search']
@@ -102,6 +103,30 @@ def get_boardgame_id():
     return render_template('get_boardgame_id_2.html', title='Find Boardgame', name=name, published_by=published_by, categories=categories, description=description, expansions=expansions)
   else:
     return render_template('get_boardgame_id.html', title='Find Boardgame')
+
+@app.route('/get_boardgame_id/<int:id>', methods=['GET'])
+def get_boardgame_id2(id):
+    search_id = str(id)
+    print("searched for a game with id " + search_id) 
+    r = requests.get("http://127.0.0.1:8000/details/" + search_id)
+
+    if(r.status_code == 404):
+        return render_template('Game404.html', title='Find Boardgame',value=search_id)
+
+    game = r.json()
+    for key in game.keys():
+        if key == 'Name':
+            name = str(game[key])
+        if key == 'Publisher':
+            published_by = game[key]
+        if key == 'Category':
+            categories = game[key]
+        if key == 'Description':
+            description = str(game[key])
+        if key == 'Expansion':
+            expansions = game[key]
+    return render_template('get_boardgame_id_2.html', title='Find Boardgame', name=name, published_by=published_by, categories=categories, description=description, expansions=expansions)
+
 
 @app.route('/get_boardgame_name', methods=['GET', 'POST'])
 def get_boardgame_name():
